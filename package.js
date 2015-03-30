74a4dd399ec0aea8b6f6bcb49ada9617c20ebebe
Dean Radcliffe <dradcliffe@opinionlab.com>
2015-03-29 22:39:15 -0500

    debugOnly flag

--- package.js
+++ package.js
@@ -1,13 +1,10 @@
 Package.describe({
   name: 'deanius:debug-ddp',
   version: '1.0.5',
-  // Brief, one-line summary of the package.
   summary: 'Shows all of your DDP Messages in your browser console.',
-  // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/chicagogrooves/meteor-debug-ddp.git',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: 'README.md',
+  debugOnly: true
 });
 
 Package.onUse(function(api) {
